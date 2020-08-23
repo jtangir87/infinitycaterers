55e3f9341ff8d4c9662551b549548cf7299b9387
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-08-23 18:32:07 -0400

    about us, mitzvah and wedding menus

--- pages/urls.py
+++ pages/urls.py
@@ -9,7 +9,23 @@ urlpatterns = [
     path("contact", ContactUsView.as_view(), name="contact_us"),
     path("contact-submit", contact_us_form, name="contact_us_form"),
     path(
+        "about-us",
+        TemplateView.as_view(template_name="pages/about_us.html"),
+        name="about_us",
+    ),
+    path(
         "venues", TemplateView.as_view(template_name="pages/venues.html"), name="venues"
+    ),
+    ## MENUS ##
+    path(
+        "menus/wedding",
+        TemplateView.as_view(template_name="pages/menus/wedding.html"),
+        name="menu_wedding",
+    ),
+    path(
+        "menus/mitzvah",
+        TemplateView.as_view(template_name="pages/menus/mitzvah.html"),
+        name="menu_mitzvah",
     ),
 ]
 
